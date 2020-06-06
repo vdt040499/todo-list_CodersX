94b85fdd1f840fdc081714efad9c715fa40fe176
VoTan <vdt040499@gmail.com>
2020-06-06 13:25:54 +0700

    6 Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
