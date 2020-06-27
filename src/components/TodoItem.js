7682656482acab813678aea314f5b59243cd7049
VoTan <vdt040499@gmail.com>
2020-06-27 12:32:20 +0700

    Dynamic class names using classnames module

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import classNames from 'classnames';
+
 import './TodoItem.css';
 class TodoItem extends React.Component {
     render() {
         const { item } = this.props;
-        let className = 'TodoItem';
 
-        if (item.isComplete) {
-            className += ' TodoItem-complete';
-        }
+        //1st way
+        // let className = 'TodoItem';
+
+        // if (item.isComplete) {
+        //     className += ' TodoItem-complete';
+        // }
+
+        //2nd way using classnames
+
+        let className = classNames({
+            TodoItem: true,
+            'TodoItem-complete': item.isComplete
+        });
 
         return  (
             <div className= {className}>
